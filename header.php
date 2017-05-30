3bb21ffa4ee3009a06088d67695e8103619e404c
Jacob Bracket <jacob.brackett@me.com>
2017-05-29 18:22:23 -0700

    renamed navbar.css to main.css

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <!-- Style Sheets -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/navbar.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <?php 
         # $stylesheets
         if(!empty($stylesheets)){
